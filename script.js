fafee22ea48a2b90e8a944978ab37854038e4445
oussama <64930789+oussama-fellag@users.noreply.github.com>
2024-01-16 17:12:03 +0100

    win page add

--- script.js
+++ script.js
@@ -16,24 +16,6 @@ document.addEventListener("DOMContentLoaded", function () {
     // Shuffle puzzle pieces (optional - remove this line if you want to use the initial configuration as is)
     shuffleTiles(initialConfiguration);
 });
-
-function shuffleTiles(configuration) {
-    const puzzleContainer = document.getElementById("puzzle-container");
-    const tiles = Array.from(puzzleContainer.children);
-
-    if (configuration) {
-        // Use the provided configuration
-        configuration.forEach((value, index) => {
-            tiles[index].innerText = value;
-        });
-    } else {
-        // Shuffle the tiles randomly
-        tiles.sort(() => Math.random() - 0.5);
-    }
-
-    tiles.forEach((tile) => puzzleContainer.appendChild(tile));
-}
-
 function handleTileClick(tile) {
     const puzzleContainer = document.getElementById("puzzle-container");
     const tiles = Array.from(puzzleContainer.children);
@@ -52,11 +34,30 @@ function handleTileClick(tile) {
 
         // Check if the puzzle is solved
         if (isPuzzleSolved(tiles)) {
-            alert("Congratulations! You solved the puzzle!");
-            shuffleTiles();
+            // Redirect to the winning page
+            window.location.href = "winner.html";
         }
     }
 }
+
+function shuffleTiles(configuration) {
+    const puzzleContainer = document.getElementById("puzzle-container");
+    const tiles = Array.from(puzzleContainer.children);
+
+    if (configuration) {
+        // Use the provided configuration
+        configuration.forEach((value, index) => {
+            tiles[index].innerText = value;
+        });
+    } else {
+        // Shuffle the tiles randomly
+        tiles.sort(() => Math.random() - 0.5);
+    }
+
+    tiles.forEach((tile) => puzzleContainer.appendChild(tile));
+}
+
+
 
 function isAdjacent(tile1, tile2) {
     const tile1Index = Array.from(tile1.parentNode.children).indexOf(tile1);
